7060c88b955a9f83a8f2b6b45e5debc7157e0bbb
joswinkj <joswin@ideas2it.com>
2016-03-18 19:09:43 +0530

    Works on 18th March

--- name_job_extraction/matcher_funs.py
+++ name_job_extraction/matcher_funs.py
@@ -1,7 +1,9 @@
 import re
 from sets import Set
 
-DEBUG = False
+import pdb
+import sys
+import pandas as pd
 
 def parse_string(x) :
     '''
@@ -16,16 +18,24 @@ def parse_string(x) :
     else :
         raise Exception()
 
-class Matcher:
+class Matcher(object):
+    def __init__(self):
+        pass
 
-    @staticmethod
-    def match_names_and_jobs( text, names, jobs):
+    def match_names_and_jobs(self, text, names, jobs):
         '''
         :param text: text
         :param names: list of names
         :param jobs: list of jobs
         :return: name and job matches
         '''
+        tmp = pd.read_csv('../constant_files.csv',index_col=0)
+        DEBUG = tmp.loc['DEBUG','value']
+        if DEBUG:
+            orig_stdout = sys.stdout
+            f = file('logfile.txt', 'w')
+            sys.stdout = f
+
         names = list([x for x in names if x not in jobs])
         names = list(Set([x for x in names]))
         jobs = list(Set([x for x in jobs]))
@@ -69,7 +79,7 @@ class Matcher:
                 name = names_dict[name_start_index][0]
                 distance = None
                 if job_start_index < name_start_index:
-                    distance = name_start_index - job_end_index
+                    distance = 99999999
                 else:
                     distance = job_start_index - name_end_idex
                 assert distance is not None
@@ -78,6 +88,15 @@ class Matcher:
                     print "Name: %s, Job: %s, Distance %s" % (
                         name, job, distance
                     )
+        job_and_names = self.matching_fun_orig(jobs_dict,names_dict,distances,sorted_job_indexes,sorted_name_indexes,DEBUG)
+        if DEBUG:
+            print "*" * 100
+            sys.stdout = orig_stdout
+            f.close()
+
+        return job_and_names
+
+    def matching_fun_orig(self,jobs_dict,names_dict,distances,sorted_job_indexes,sorted_name_indexes,DEBUG=False):
         if DEBUG:
             print "*" * 100
         job_and_names = []
@@ -90,7 +109,61 @@ class Matcher:
         n = len(sorted_name_indexes)
         if DEBUG:
             print "*" * 100
-        while (min_distance <= 4*first_min_distance or min_distance > 10) \
+        while (min_distance <= 4*first_min_distance or min_distance <= first_min_distance+12) \
+              and min_distance <= 50 \
+                and min_distance <= max_distance:
+            ind = distances.index(min_distance)
+            j = ind % n #j indicate name position in the sorted_name_indexes list
+            i = ind / n #i indicate job position in the sorted_job_indexes list
+            if len(sorted_job_indexes) <= i or \
+                    len(sorted_name_indexes) <= j:
+                print "min_distance", min_distance
+                print "ind", ind
+                print "n", n
+                print "sorted_job_indexes", len(sorted_job_indexes)
+                print "sorted_name_indexes", len(sorted_name_indexes)
+                print distances
+                assert False
+            name_index = sorted_name_indexes[j]
+            name = names_dict[name_index][0]
+            job_index = sorted_job_indexes[i]
+            job = jobs_dict[job_index][0]
+            if DEBUG:
+                print "*" * 100
+                print "Name: %s, Job: %s, Distance %s" % (
+                    name, job, min_distance
+                )
+            job_and_names.append((
+                parse_string(
+                    re.sub(r"^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$", "",  job)
+                ),
+                parse_string(
+                    re.sub(r"^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$", "",  name)
+                )
+            ))
+            # pdb.set_trace()
+            # for k in range(0, m):
+            #     distances[k*n+j] = max_distance + 1
+            for k in range(0, n):
+                distances[i*n+k] = max_distance + 1
+            min_distance = min(distances)
+
+        return job_and_names
+
+    def matching_fun_bck(self,jobs_dict,names_dict,distances,sorted_job_indexes,sorted_name_indexes,DEBUG=False):
+        if DEBUG:
+            print "*" * 100
+        job_and_names = []
+        if len(distances) == 0:
+            return job_and_names
+        max_distance = max(distances)
+        min_distance = min(distances)
+        first_min_distance = min_distance
+        m = len(sorted_job_indexes)
+        n = len(sorted_name_indexes)
+        if DEBUG:
+            print "*" * 100
+        while (min_distance <= 4*first_min_distance ) \
               and min_distance <= 50 \
                 and min_distance <= max_distance:
             ind = distances.index(min_distance)
@@ -126,6 +199,5 @@ class Matcher:
             for k in range(0, n):
                 distances[i*n+k] = max_distance + 1
             min_distance = min(distances)
-        if DEBUG:
-            print "*" * 100
+
         return job_and_names
