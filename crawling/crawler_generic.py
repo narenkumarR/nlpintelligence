6732be05b1ec2339427a2d4c8620833f790766b7
joswinkj <joswin@ideas2it.com>
2016-04-23 15:40:00 +0530

    forgot to add crawler_generic.py file

---
+++ crawling/crawler_generic.py
@@ -0,0 +1,197 @@
+__author__ = 'joswin'
+
+import datetime
+import time
+import logging
+import re
+import os
+import pickle
+from random import shuffle
+
+import crawler
+
+class LinkedinCrawlerThread(object):
+    def __init__(self):
+        '''
+        '''
+        pass
+
+    def get_finished_links(self,var_name='Linkedin URL',*args):
+        link_list = []
+        for f_name in args:
+            with open(f_name,'r') as f:
+                for line in f:
+                    tmp = eval(line)
+                    link_list.append(tmp[var_name])
+        return list(set(link_list))
+
+    def get_files_in_dir(self,dir_path='.',remove_regex = '',match_regex=''):
+        ''' get matching files
+        :param dir_path:
+        :param remove_regex:
+        :param match_regex:
+        :return:
+        '''
+        files = os.listdir(dir_path)
+        if dir_path == '.':
+            dir_path = ''
+        if remove_regex:
+            files = [i for i in files if not re.search(remove_regex,i)]
+        if match_regex:
+            files = [i for i in files if re.search(match_regex,i)]
+        files = [dir_path+i for i in files]
+        return files
+
+    def get_connected_urls(self,name_list,field_list,page_field='Linkedin Page',
+                           remove_regex = r'\?trk=pub-pbmap|\?trk=prof-samename-picture|\?trk=extra_biz_viewers_viewed'):
+        '''
+        :param name_list:
+        :return:
+        '''
+        out_list = []
+        for name in name_list:
+            with open(name,'r') as f_in:
+                for line in f_in:
+                    tmp = eval(line)
+                    for field in field_list:
+                        if field in tmp:
+                            for det in tmp[field]:
+                                out_list.append(re.sub(remove_regex,'',det[page_field]))
+        out_list = list(set(out_list))
+        return out_list
+
+    def run_organization_crawler_single(self,res_file=None,log_file=None,crawled_loc='crawled_res/',browser='Firefox',
+                                        visible=False,proxy=True,
+                                 url_file='company_urls_to_crawl_18April.pkl',limit_no=60000,n_threads=6):
+        '''
+        :param res_file:
+        :param log_file:
+        :param crawled_loc:
+        :param cc: the crawler object
+        :param url_file:
+        :param limit_no:
+        :return:
+        '''
+        if res_file is None:
+            res_file = crawled_loc+'company_crawled_'+str(datetime.date.today())+'.txt'
+        if log_file is None:
+            log_file = crawled_loc+'logs/company_crawling'+str(datetime.date.today())+'.log'
+        crawled_files = self.get_files_in_dir(crawled_loc,match_regex='^company.+\.txt$')
+        finished_urls = self.get_finished_links('Linkedin URL',*crawled_files)
+        with open(url_file,'r') as f:
+            urls = pickle.load(f)
+        connected_urls = self.get_connected_urls(crawled_files,['Also Viewed Companies'],page_field='company_linkedin_url')
+        urls = urls+connected_urls
+        urls = list(set(urls)-set(finished_urls))
+        shuffle(urls)
+        urls = urls[:limit_no]
+        cc = crawler.LinkedinCompanyCrawlerThread(browser,visible=visible,proxy=proxy)
+        cc.run(urls,res_file,log_file,n_threads)
+
+    def run_organization_crawler_auto(self,crawled_loc='crawled_res/',browser = 'Firefox',visible=False,
+                                 proxy=True,url_file='company_urls_to_crawl_18April.pkl',limit_no=30000,n_threads=6,
+                                 ):
+        '''Need work
+        :param crawled_loc:
+        :param browser:
+        :param visible:
+        :param proxy:
+        :param url_file:
+        :param limit_no:
+        :param n_threads:
+        :return:
+        '''
+        cc = crawler.LinkedinCompanyCrawlerThread(browser,visible=visible,proxy=proxy)
+        while True:
+            try:
+                print(datetime.datetime.now())
+                res_file = crawled_loc+'company_crawled_'+str(datetime.date.today())+'.txt'
+                log_file = crawled_loc+'logs/company_crawling'+str(datetime.date.today())+'.log'
+                crawled_files = self.get_files_in_dir(crawled_loc,match_regex='^company.+\.txt$')
+                finished_urls = self.get_finished_links('Linkedin URL',*crawled_files)
+                print('finished urls:{}'.format(len(finished_urls)))
+                with open(url_file,'r') as f:
+                    urls = pickle.load(f)
+                connected_urls = self.get_connected_urls(crawled_files,['Also Viewed Companies'],page_field='company_linkedin_url')
+                print('connected urls:{}'.format(len(connected_urls)))
+                urls = urls+connected_urls
+                urls = list(set(urls)-set(finished_urls))
+                shuffle(urls)
+                urls = urls[:limit_no]
+                print('urls to crawl:{}'.format(len(urls)))
+                del connected_urls,finished_urls
+                cc.run(urls,res_file,log_file,n_threads)
+            except:
+                logging.exception('Exception in main auto thread.wait for 10 seconds')
+                print('error. wait for 10 seconds, then try again')
+                time.sleep(10)
+                continue
+
+    def run_people_crawler_single(self,res_file=None,log_file=None,crawled_loc='crawled_res/',browser='Firefox',
+                                        visible=False,proxy=True,
+                                 url_file='people_urls_to_crawl_18April.pkl',limit_no=60000,n_threads=6):
+        '''
+        :param res_file:
+        :param log_file:
+        :param crawled_loc:
+        :param cc: the crawler object
+        :param url_file:
+        :param limit_no:
+        :return:
+        '''
+        if res_file is None:
+            res_file = crawled_loc+'people_crawled_'+str(datetime.date.today())+'.txt'
+        if log_file is None:
+            log_file = crawled_loc+'logs/people_crawling'+str(datetime.date.today())+'.log'
+        crawled_files = self.get_files_in_dir(crawled_loc,match_regex='^people.+\.txt$')
+        finished_urls = self.get_finished_links('Linkedin URL',*crawled_files)
+        with open(url_file,'r') as f:
+            urls = pickle.load(f)
+        connected_urls = self.get_connected_urls(crawled_files,['Related People','Same Name People'],
+                                                 page_field='Linkedin Page')
+        urls = urls+connected_urls
+        urls = list(set(urls)-set(finished_urls))
+        shuffle(urls)
+        urls = urls[:limit_no]
+        cc = crawler.LinkedinProfileCrawlerThread(browser,visible=visible,proxy=proxy)
+        cc.run(urls,res_file,log_file,n_threads)
+
+    def run_people_crawler_auto(self,crawled_loc='crawled_res/',browser = 'Firefox',visible=False,
+                                 proxy=True,url_file='people_urls_to_crawl_18April.pkl',limit_no=30000,n_threads=6,
+                                 ):
+        '''Need work
+        :param crawled_loc:
+        :param browser:
+        :param visible:
+        :param proxy:
+        :param url_file:
+        :param limit_no:
+        :param n_threads:
+        :return:
+        '''
+        cc = crawler.LinkedinProfileCrawlerThread(browser,visible=visible,proxy=proxy)
+        while True:
+            try:
+                print(datetime.datetime.now())
+                res_file = crawled_loc+'people_crawled_'+str(datetime.date.today())+'.txt'
+                log_file = crawled_loc+'logs/people_crawling'+str(datetime.date.today())+'.log'
+                crawled_files = self.get_files_in_dir(crawled_loc,match_regex='^people.+\.txt$')
+                finished_urls = self.get_finished_links('Linkedin URL',*crawled_files)
+                print('finished urls:{}'.format(len(finished_urls)))
+                with open(url_file,'r') as f:
+                    urls = pickle.load(f)
+                connected_urls = self.get_connected_urls(crawled_files,['Related People','Same Name People'],
+                                                         page_field='Linkedin Page')
+                print('connected urls:{}'.format(len(connected_urls)))
+                urls = urls+connected_urls
+                urls = list(set(urls)-set(finished_urls))
+                shuffle(urls)
+                urls = urls[:limit_no]
+                del connected_urls,finished_urls
+                print('urls to crawl:{}'.format(len(urls)))
+                cc.run(urls,res_file,log_file,n_threads)
+            except:
+                logging.exception('Exception in main auto thread.wait for 10 seconds')
+                print('error. wait for 10 seconds, then try again')
+                time.sleep(10)
+                continue
--- network_analysis/test.py
+++ network_analysis/test.py
@@ -1,25 +1,34 @@
 __author__ = 'joswin'
 
 import re
+with open('company_crawled_15April_2.txt','r') as f:
+    tmp = f.readlines()
 
+with open('company_crawled_2016-04-22.txt','r') as f:
+    tmp1 = f.readlines()
+
+tmp = tmp+tmp1
+del tmp1
 node_list = []
 edge_list = []
-with open('company_crawled_2016-04-22.txt','r') as f:
-    for line in f:
-        tmp = eval(line)
-        main_url = tmp['Linkedin URL']
-        node_list.append(main_url)
-        if 'Also Viewed Companies' in tmp:
-            for i in tmp['Also Viewed Companies']:
-                link = i['company_linkedin_url']
-                link = re.sub(r'\?trk=pub-pbmap|\?trk=prof-samename-picture|\?trk=extra_biz_viewers_viewed','',link)
-                node_list.append(link)
-                if main_url == link:
-                    continue
-                elif main_url < link:
-                    edge_list.append((main_url,link))
-                else:
-                    edge_list.append((link,main_url))
+from random import shuffle
+shuffle(tmp)
+f=tmp[:200]
+for line in f:
+    tmp = eval(line)
+    main_url = tmp['Linkedin URL']
+    node_list.append(main_url)
+    if 'Also Viewed Companies' in tmp:
+        for i in tmp['Also Viewed Companies']:
+            link = i['company_linkedin_url']
+            link = re.sub(r'\?trk=pub-pbmap|\?trk=prof-samename-picture|\?trk=extra_biz_viewers_viewed','',link)
+            node_list.append(link)
+            if main_url == link:
+                continue
+            elif main_url < link:
+                edge_list.append((main_url,link))
+            else:
+                edge_list.append((link,main_url))
 
 node_list = list(set(node_list))
 edge_list = list(set(edge_list))
@@ -28,3 +37,81 @@ import networkx as nx
 G=nx.Graph()
 G.add_nodes_from(node_list)
 G.add_edges_from(edge_list)
+
+pos=nx.fruchterman_reingold_layout(G)
+
+Xv=[pos[k][0] for k in node_list]
+Yv=[pos[k][1] for k in node_list]
+
+Xed=[]
+Yed=[]
+for edge in edge_list:
+    Xed+=[pos[edge[0]][0],pos[edge[1]][0], None]
+    Yed+=[pos[edge[0]][1],pos[edge[1]][1], None]
+
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+trace3=Scatter(x=Xed,
+               y=Yed,
+               mode='lines',
+               line=Line(color='rgb(210,210,210)', width=1),
+               hoverinfo='none'
+               )
+trace4=Scatter(x=Xv,
+               y=Yv,
+               mode='markers',
+               name='net',
+               marker=Marker(symbol='dot',
+                             size=5,
+                             color='#6959CD',
+                             line=Line(color='rgb(50,50,50)', width=0.5)
+                             ),
+               text=node_list,hoverinfo='text')
+annot="This networkx.Graph has the Fruchterman-Reingold layout<br>Code:"+\
+"<a href='http://nbviewer.ipython.org/gist/empet/07ea33b2e4e0b84193bd'> [2]</a>"
+
+data1=Data([trace3, trace4])
+width=800
+height=800
+axis=dict(showline=False, # hide axis line, grid, ticklabels and  title
+          zeroline=False,
+          showgrid=False,
+          showticklabels=False,
+          title=''
+          )
+layout=Layout(title= "company graph",
+    font= Font(size=12),
+    showlegend=False,
+    autosize=False,
+    width=width,
+    height=height,
+    xaxis=XAxis(axis),
+    yaxis=YAxis(axis),
+    margin=Margin(
+        l=40,
+        r=40,
+        b=85,
+        t=100,
+    ),
+    hovermode='closest',
+    annotations=Annotations([
+           Annotation(
+           showarrow=False,
+            text='This igraph.Graph has the Kamada-Kawai layout',
+            xref='paper',
+            yref='paper',
+            x=0,
+            y=-0.1,
+            xanchor='left',
+            yanchor='bottom',
+            font=Font(
+            size=14
+            )
+            )
+        ]),
+    )
+data1=Data([trace3, trace4])
+fig1=Figure(data=data1, layout=layout)
+fig1['layout']['annotations'][0]['text']=annot
+py.iplot(fig1, filename='company-network-nx')
