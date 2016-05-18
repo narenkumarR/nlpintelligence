aac8e751ce4e6cee1a62e61103a35b5fe9f416b3
joswinkj <joswin@ideas2it.com>
2016-05-18 20:48:44 +0530

    Feature: implemented queue based processing for email classification model integration fixes : in text processing part of classification model

---
+++ email_models_api/sqs_main.py
@@ -0,0 +1,25 @@
+__author__ = 'joswin'
+import boto3
+import json
+from sqs_credentials import region_name,aws_access_key_id,aws_secret_access_key,in_queue_name,out_queue_name
+from intent_class_models.naive_bayes.naive_bayes_model import NaiveBayesModel
+
+nb = NaiveBayesModel()
+nb.load_model()
+
+sqs = boto3.resource('sqs',region_name=region_name,aws_access_key_id=aws_access_key_id,
+                     aws_secret_access_key=aws_secret_access_key)
+in_queue = sqs.get_queue_by_name(QueueName=in_queue_name)
+out_queue = sqs.get_queue_by_name(QueueName=out_queue_name)
+
+max_queue_messages = 10
+
+while True:
+    for message in in_queue.receive_messages(MaxNumberOfMessages=max_queue_messages):
+        # pdb.set_trace()
+        message_id = 'no id'
+        body_json = json.loads(message.body)
+        prediction = str(nb.predict_textinput(body_json['text'])[0])
+        out_json = '"id":{},"prediction":{}'.format(body_json['id'],prediction)
+        out_queue.send_message(MessageBody=out_json)
+        message.delete()
