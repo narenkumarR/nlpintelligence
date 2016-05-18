98749c86f63385238a2532c7fdef94a6ca10113e
joswinkj <joswin@ideas2it.com>
2016-05-18 20:48:44 +0530

    Feature: implemented queue based processing for email classification model integration fixes : in text processing part of classification model

---
+++ email_models_api/sqs_credentials.py
@@ -0,0 +1,7 @@
+__author__ = 'joswin'
+region_name='ap-southeast-1'
+aws_access_key_id='AKIAJUDP7FRPRTLTANWA'
+aws_secret_access_key='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'
+
+in_queue_name = 'intelligence_in_dev'
+out_queue_name = 'intelligence_out_dev'
