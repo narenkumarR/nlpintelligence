e5922f3c483b6bd54b3cfa54b69f54a4e0a78a1b
joswinkj <joswin@ideas2it.com>
2017-01-03 15:06:02 +0530

    Initial codes before jan3

---
+++ linkedin_company_crawler.py
@@ -0,0 +1,283 @@
+"""
+File : linkedin_organization_service.py
+Created On: 23-Sep-2015
+Author: ideas2it
+"""
+import re
+from bs_crawl import BeautifulsoupCrawl
+# import linkedin_parser
+import selenium_crawl
+import logging
+from selenium.common.exceptions import TimeoutException
+from httplib import CannotSendRequest,BadStatusLine
+from socket import error as socket_error
+
+from linkedin_login import login_fun
+
+all_org_cases = ['Specialties','Website','Industry','Type','Headquarters','Company Size','Founded',
+                     'Company Name','Description Text','Employee Details','Also Viewed Companies','Linkedin URL']
+
+
+def dec_fun(fn):
+    '''
+    not used
+    if fn executions into error, None is returned
+    :param fn:
+    :return:
+    '''
+    def new_fun(*args,**kwargs):
+        try:
+            return fn(*args,**kwargs)
+        except Exception as e:
+            logging.exception('Exception while fetching details from company page ')
+            return None
+    return new_fun
+
+def complete_cases_org(fn):
+    '''
+    not used
+    :param fn:
+    :return:
+    '''
+    def new_fun1(*args,**kwargs):
+        try:
+            out = fn(*args,**kwargs)
+            out1 = {}
+            for i in all_org_cases:
+                try:
+                    out1[i] = out[i]
+                except:
+                    out1[i] = ''
+            return out1
+        except Exception as e:
+            logging.exception('Exception while running main from company page ')
+            return None
+    return new_fun1
+
+
+
+class LinkedinOrganizationService(object):
+    def __init__(self,browser='Firefox',visible = True,proxy=False,proxy_ip = None,proxy_port = None,use_tor=None,
+                 login=False):
+        ''' support for methods other than selenium needs fixes
+        :param browser:
+        :param visible:
+        :param proxy:
+        :param proxy_ip:
+        :param proxy_port:
+        :param use_tor:
+        :return:
+        '''
+        # print('class intializing')
+        self._crawler = BeautifulsoupCrawl.get_soup
+        self.browser = browser
+        self.visible = visible
+        self.proxy = proxy
+        self.proxy_ip = proxy_ip
+        self.proxy_port = proxy_port
+        self.use_tor = use_tor
+        self.login = login
+        self.init_selenium_parser(browser,visible,proxy,proxy_ip,proxy_port,use_tor,login)
+
+    def exit(self):
+        self.link_parser.exit()
+
+    def init_selenium_parser(self,browser=None,visible = None,proxy=None,proxy_ip = None,proxy_port = None,
+                             use_tor=None,login=False):
+        '''
+        :param browser:
+        :param visible:
+        :param proxy:
+        :param proxy_ip:
+        :param proxy_port:
+        :param use_tor:
+        :param login:
+        :return:
+        '''
+        if browser is None and visible is None and visible is None and proxy is None and use_tor is None: #if no input reload same parser
+            # self.link_parser = linkedin_parser.LinkedinParserSelenium(self.browser,visible=self.visible,proxy=self.proxy,
+            #                                                           proxy_ip=self.proxy_ip,proxy_port=self.proxy_port,
+            #                                                           use_tor=self.use_tor)
+            self.link_parser = selenium_crawl.SeleniumParser(self.browser,visible=self.visible,proxy=self.proxy,
+                                                                      proxy_ip=self.proxy_ip,proxy_port=self.proxy_port,
+                                                                      use_tor=self.use_tor)
+        else:
+            try:  # try with inputs. if success, update the class variables. if error, try with already existing class vars
+                # self.link_parser = linkedin_parser.LinkedinParserSelenium(browser,visible=visible,proxy=proxy,
+                #                                                           proxy_ip=proxy_ip,proxy_port=proxy_port,use_tor=use_tor)
+                self.link_parser = selenium_crawl.SeleniumParser(browser,visible=visible,proxy=proxy,
+                                                                          proxy_ip=proxy_ip,proxy_port=proxy_port,use_tor=use_tor)
+                self.browser = browser
+                self.visible = visible
+                self.proxy = proxy
+                self.proxy_ip = proxy_ip
+                self.proxy_port = proxy_port
+                self.use_tor = use_tor
+            except:
+                # self.link_parser = linkedin_parser.LinkedinParserSelenium(self.browser,visible=self.visible,proxy=self.proxy,
+                #                                                   proxy_ip=self.proxy_ip,proxy_port=self.proxy_port,use_tor=self.use_tor)
+                self.link_parser = selenium_crawl.SeleniumParser(self.browser,visible=self.visible,proxy=self.proxy,
+                                                                  proxy_ip=self.proxy_ip,proxy_port=self.proxy_port,use_tor=self.use_tor)
+        if login:
+            login_fun(self.link_parser.browser)
+
+
+    # @complete_cases_org
+    def get_organization_details_from_linkedin_link(self,url,use_selenium=True):
+        '''
+        :param url: linkedin company url
+        :return:
+        '''
+        try:
+            self.details = {'Linkedin URL':url,'Original URL':url}
+            if use_selenium:
+                self.soup = self.link_parser.get_soup(url)
+                if re.search(r'the company you are looking for is not active|the company you are looking for does not exist',self.soup.text):
+                    self.details['Notes'] = 'Company page not found'
+                    return self.details
+                redirect_url = self.link_parser.browser.current_url
+                if '?trk=login_reg_redirect' in redirect_url:
+                    redirect_url = url
+            else:
+                self.soup = self._crawler(url)
+                redirect_url = url
+            try:
+                if 'Largest Professional Network' in self.soup.title.text:
+                    self.details['Notes'] = 'Not Available Pubicly'
+                    return self.details
+            except:
+                self.details['Notes'] = 'Java script code'
+                return self.details
+                # logging.info(self.soup)
+            # else:
+            #     self.details['Notes'] = 'Publicaly available'
+            redirect_url = re.split('\?trk',redirect_url)[0]
+            self.details['Linkedin URL'] = redirect_url
+            self.get_name()
+            self.get_description()
+            self.get_details()
+            self.get_employees()
+            self.get_also_viewed()
+            return self.details
+        except TimeoutException:
+            logging.error('Time out exception for url: '+url)
+            return None
+        except socket_error:
+            logging.error('Socket error for url:'+url)
+            return None
+        except CannotSendRequest:
+            logging.error('Cannot send request error for url:{}'.format(url))
+            return None
+        except BadStatusLine:
+            logging.error('Badstatus line error for url:{}'.format(url))
+            return None
+        except Exception as e:
+            logging.exception('Exception while running main from company page for url:'+url)
+            return self.details
+
+    # @dec_fun
+    def get_description(self):
+        '''
+        :return:
+        '''
+        try:
+            para = self.soup.find("div", {"class": "basic-info-description"}).findChild().getText()
+            para = re.sub(r'[.]+','. ',re.sub(r'[\r\n]','.',para))
+            self.details['Description Text'] = para
+        except:
+            self.details['Description Text'] = ''
+
+    # @dec_fun
+    def get_name(self):
+        '''
+        :return:
+        '''
+        try:
+            self.details['Company Name'] = self.soup.find("div",{"class":"content-wrapper"}).find("h1").findChild().getText().strip()
+        except:
+            return
+
+    # @dec_fun
+    def get_details(self):
+        '''
+        :return:
+        '''
+        try:
+            headers = ['h3','h4']
+            for header in headers:
+                for tag in self.soup.find("div",{"class":"basic-info-about"}).findAll(header):
+                    try:
+                        self.details[tag.getText().strip()] = tag.find_next().getText().strip()
+                    except:
+                        continue
+        except:
+            return
+    # @dec_fun
+    def get_employees(self):
+        '''
+        :return:
+        '''
+        # logging.info('get_employees started')
+        try:
+            p_list = self.soup.find('div',{'class':'company-employees module'}).findAll('li')
+            # logging.info('plist length:{}'.format(len(p_list)))
+        except:
+            try:
+                logging.info('company_part:employee_details went to exception. try the logging option')
+                p_list = self.soup.find('div',{'class':'company-density module'}).findAll('li')
+                logging.info('plist len loggin option 1 {}'.format(len(p_list)))
+            except:
+                logging.info('went to exception while trying loggin')
+                self.details['Employee Details'] = []
+                return
+        if not p_list:
+            logging.info('no p_list. try logging')
+            try:
+                p_list = self.soup.find('div',{'class':'company-density module'}).findAll('li')
+                logging.info('p list logging opiton :{}'.format(len(p_list)))
+            except:
+                logging.info('went to exception while trying loggin 2')
+                self.details['Employee Details'] = []
+                return
+        out_list = []
+        for tmp in p_list:
+            tmp_dic = {}
+            try:
+                tmp_dic['linkedin_url'] = tmp.find('a')['href']
+            except:
+                tmp_dic['linkedin_url'] = ''
+            try:
+                tmp_dic['Name'] = tmp.find('dt').text
+            except:
+                tmp_dic['Name'] = ''
+            try:
+                tmp_dic['Designation'] = tmp.find('dd').text
+            except:
+                tmp_dic['Designation'] = ''
+            out_list.append(tmp_dic)
+        # logging.info('out_list : {}'.format(out_list))
+        self.details['Employee Details'] = out_list
+
+    # @dec_fun
+    def get_also_viewed(self):
+        '''
+        :return:
+        '''
+        try:
+            p_list = self.soup.find('div',{'class':'also-viewed module'}).findAll('li')
+        except:
+            self.details['Also Viewed Companies'] = []
+            return
+        out_list = []
+        for tmp in p_list:
+            tmp_dic = {}
+            try:
+                tmp_dic['company_linkedin_url'] = tmp.find('a')['href']
+            except:
+                tmp_dic['company_linkedin_url'] = ''
+            try:
+                tmp_dic['Company Name'] = tmp.find('a').find('img')['alt']
+            except:
+                tmp_dic['Company Name'] = ''
+            out_list.append(tmp_dic)
+        self.details['Also Viewed Companies'] = out_list
---
+++ utils.py
@@ -0,0 +1,48 @@
+__author__ = 'joswin'
+from url_cleaner import UrlCleaner
+
+class SoupUtils(object):
+
+    def __init__(self):
+        self.url_cleaner = UrlCleaner()
+
+    def get_text_from_soup(self,soup):
+        '''
+        :param soup:
+        :return:
+        '''
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+        # get text
+        text = soup.get_text(' ')
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+        return text
+
+    def get_all_links_soupinput(self,soup,base_url,merge_urls=True,only_visible=True):
+        '''
+        :param soup:
+        :param base_url:
+        :param merge_urls:
+        :return:
+        '''
+        if only_visible:
+            [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+        url_list, mail_list = [],[]
+        url_tmp = []
+        for a in soup.find_all('a', href=True):
+            url = a['href']
+            if '@' in url:
+                mail_list.append(url)
+            else:
+                if merge_urls:
+                    url = self.url_cleaner.merge_urls(base_url,url)
+                url = self.url_cleaner.clean_url(url)
+                if url not in url_tmp:
+                    url_list.append((url,a.text))
+                    url_tmp.append(url)
+        return url_list,mail_list
